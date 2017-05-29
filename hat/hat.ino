711f5943675894fdb3fcb09383bbd1afbd870051
mytchj <mytchj@gmail.com>
2017-05-28 23:49:10 -0700

    Awesome Hats

--- hat/hat.ino
+++ hat/hat.ino
@@ -1,46 +1,91 @@
 #include <Adafruit_NeoPixel.h>
 #include "hsv2rgb.h"
-#include "substrip.h"
 
 #define PIN            1
-#define NUMPIXELS      5
+#define NUMPIXELS      100
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 500; // delay for half a second
+int delayval = 200; // delay for half a second
 
 void setup() {
   pixels.begin();
 }
 
-
-substrip left = makeSubstrip(0, 1);
-substrip right = makeSubstrip(2, 4);
-
 void loop() {
-  pixels.clear();
-  setSubstrip(pixels, left, 100, 0, 0);
-  setSubstrip(pixels, right, 0, 0, 100);
-  pixels.show();
-  delay(delayval);
-
-//  hsv chsv = {random(0, 360), random(50, 100) / 100.0, random(50, 100) / 100.0};
-//  rgb crgb = hsv2rgb(chsv);
-//  uint32_t cColor = pixels.Color(crgb.r * 255, crgb.g * 255, crgb.b * 255);
-//  
-//  delayval -= 20;
-//  if(delayval == 40)
-//    delayval = 300;
-//  
-//  for(int i=0;i<NUMPIXELS;i++){
-//    pixels.setPixelColor(i, cColor);
-//    pixels.show(); // This sends the updated pixel color to the hardware.
-//    delay(delayval); // Delay for a period of time (in milliseconds).
-//    pixels.clear();
-//  }
-//  for(int i=NUMPIXELS-2;i>0;i--){
-//    pixels.setPixelColor(i, cColor);
-//    pixels.show(); // This sends the updated pixel color to the hardware.
-//    delay(delayval); // Delay for a period of time (in milliseconds).
-//    pixels.clear();
-//  }
+  decrementDelay();
+  int x = random(1, 4);
+  if (x == 1)
+    snake(random(1, 10));
+  else if (x == 2)
+    line();
+  else if (x == 3)
+    fade();
 }
+
+void fade() {
+  uint32_t cColor = randColor();
+  setSubstrip(&pixels, 0, NUMPIXELS-1, cColor);
+  swc();
+  swc();
+}
+
+void line() {
+  for(int i=0; i<NUMPIXELS / 10; i++){
+    uint32_t cColor = randColor();
+    setSubstrip(&pixels, i*10, i*10 + 9, cColor);
+    swc();
+  }
+  for(int i=(NUMPIXELS / 10) - 2; i>0; i--){
+    uint32_t cColor = randColor();
+    setSubstrip(&pixels, i*10, i*10 + 9, cColor);
+    swc();
+  }
+}
+
+void snake(int n) {
+  for(int i=0; i<NUMPIXELS / n; i++){
+    uint32_t cColor = randColor();
+    setSubstrip(&pixels, i * n, i * n + n - 1, cColor);
+    swc();
+  }
+  for(int i=(NUMPIXELS / n) - 2; i>0; i--){
+    uint32_t cColor = randColor();
+    setSubstrip(&pixels, i * n, i * n + n-1, cColor);
+    swc();
+  }
+}
+
+
+uint32_t randColor() {
+  hsv chsv = {random(0, 360), random(50, 100) / 100.0, random(50, 100) / 100.0};
+  rgb crgb = hsv2rgb(chsv);
+  return pixels.Color(crgb.r * 255, crgb.g * 255, crgb.b * 255);
+}
+
+void swc() {
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delayval); // Delay for a period of time (in milliseconds).
+  pixels.clear();
+}
+
+void decrementDelay() {
+  if (delayval > 200)
+    delayval -= 40;
+  
+  if (delayval > 100)
+    delayval -= 10;
+  delayval -= 10;
+  
+  if(delayval == 0)
+    delayval = 300;
+}
+
+
+void setSubstrip(Adafruit_NeoPixel *pixels, int8_t start,  int end, uint32_t cColor) {
+  for(int i = start; i <= end; i++) {
+    pixels->setPixelColor(i, cColor);
+  }
+}
+
+
+
